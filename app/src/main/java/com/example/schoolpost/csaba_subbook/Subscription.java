cf7584b2975d922b7dfeaf668044778d8981fbeb
Csaba Nagy <csabanagy@schoolpost.ca>
2018-01-29 23:18:43 -0700

    Created new layout, added listview and button

--- app/src/main/java/com/example/schoolpost/csaba_subbook/Subscription.java
+++ app/src/main/java/com/example/schoolpost/csaba_subbook/Subscription.java
@@ -1,8 +1,72 @@
 package com.example.schoolpost.csaba_subbook;
 
+import java.util.Date;
+
 /**
- * Created by Schoolpost on 2018-01-29.
+ * Created by schoo on 1/29/2018.
  */
 
 public class Subscription {
+    private String name;
+    private Date date;
+    private float cost;
+    private String comment;
+
+    Subscription(String name, Date date, float cost, String comment) {
+        this.name = name;
+        this.date = date;
+        this.cost = cost;
+        this.comment = comment;
+    }
+
+    Subscription(String name, float cost, String comment) {
+        this.name = name;
+        this.date = new Date();
+        this.cost = cost;
+        this.comment = comment;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) throws SubscriptionNameException {
+        if (name.length() <= 20) {
+            this.name = name;
+        } else {
+            throw new SubscriptionNameException();
+        }
+        this.name = name;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public float getCost() {
+        return cost;
+    }
+
+    public void setCost(float cost) throws SubscriptionCostException {
+        if (cost >= 0) {
+            this.cost = cost;
+        } else {
+            throw new SubscriptionCostException();
+        }
+        this.cost = cost;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
 }
+
---
+++ app/src/main/java/com/example/schoolpost/csaba_subbook/SubscriptionNameException.java
@@ -0,0 +1,8 @@
+package com.example.schoolpost.csaba_subbook;
+
+/**
+ * Created by Schoolpost on 2018-01-29.
+ */
+
+public class SubscriptionNameException extends Exception {
+}
